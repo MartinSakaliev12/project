bb31558fb0fbf2a9a54eefb1fb93264c43406979
Martin Sakaliev <martinsakaliev12@gmail.com>
2025-10-27 19:58:08 +0200

    Add html and css for home component

--- Nexo/src/app/app.ts
+++ Nexo/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navigation } from './features/navigation/navigation';
+import { Home } from './features/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Navigation,Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
